9138da3b38296653504b7583e6542728bea7b9d0
min1744 <alsrms1744@gmail.com>
2021-04-07 01:02:38 +0900

    Update RemoveTheSmallestNumber.java

--- src/level1/RemoveTheSmallestNumber.java
+++ src/level1/RemoveTheSmallestNumber.java
@@ -2,19 +2,28 @@ package level1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class RemoveTheSmallestNumber {
 	public static void main(String[] args) {
 		int[] arr = {2,1,3,4};
 		List<Integer> ar = new ArrayList(Arrays.asList(arr));
-		if(arr.length == 1) {
+		for(int i = 0; i < arr.length; i++){
+			ar.add(arr[i]);
+		}
+		if(arr.length < 2) {
 			ar.clear();
 			ar.add(-1);
 		} else {
-			Collections.sort(ar, Collections.reverseOrder());
-			
+			int min = Arrays.stream(arr).min().orElse(-1);
+			Iterator<Integer> it = ar.iterator();
+			while(it.hasNext()) {
+				if(it.next() == min) {
+					it.remove();
+				}
+			}
+			ar.remove(Arrays.stream(arr).min().orElse(-1));
 		}
 		ar.toArray();
 	}
